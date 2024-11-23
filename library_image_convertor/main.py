df67388720961066c589ef739291edc85a4119cb
gfxq <gofxq@icloud.com>
2024-11-23 10:37:12 +0800

    update

--- library_image_convertor/main.py
+++ library_image_convertor/main.py
@@ -1,6 +1,14 @@
 import os
 from pathlib import Path
 
+
+
+# 定义图片文件的后缀名集合
+image_extensions = {'.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.svg',
+                    '.tif', '.psb', '.psd', '.eps'}
+
+
+
 # 定义一个函数来统计后缀名
 def count_file_extensions(directory):
     # 定义一个字典来存储后缀名和相应的计数
@@ -10,7 +18,7 @@ def count_file_extensions(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
             # 获取文件的后缀名
-            extension = Path(file).suffix
+            extension = Path(file).suffix.lower
 
             # 统计后缀名的数量
             if extension in extension_count:
@@ -18,16 +26,13 @@ def count_file_extensions(directory):
             else:
                 extension_count[extension] = 1
 
-    return extension_count
+    return extension_count 
 
 # 指定要检查的目录路径
-directory_path = 'E:/字画'
+directory_path = 'E:/library_arts'
 
 # 获取后缀名统计结果
 extensions = count_file_extensions(directory_path)
-
-# 定义图片文件的后缀名集合
-image_extensions = {'.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.svg', '.tif', '.TIF', '.psb', '.psd', '.PSD'}
 
 # 过滤出非图片文件的后缀
 non_image_extensions = {ext: count for ext, count in extensions.items() if ext not in image_extensions}
@@ -38,11 +43,6 @@ print("Non-image file extensions and their counts:")
 
 import os
 from pathlib import Path
-
-# 定义图片文件的后缀名集合
-image_extensions = {'.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.svg',
-                    '.tif', '.TIF', '.psb', '.psd', '.PSD','.eps'}
-
 
 # 遍历目录下的所有文件和子目录中的文件
 for root, dirs, files in os.walk(directory_path):
